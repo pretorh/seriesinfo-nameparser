4f4bf837383d3f327c287c68050e69c0f1a6af76
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 13:23:22 +0200

    parse multiple numbers from other formats

--- tests/multi-number.pl
+++ tests/multi-number.pl
@@ -6,8 +6,9 @@ use SeriesInfo::NameParser;
 use Test::More tests => 1;
 
 subtest "get multiple numbers" => sub {
-    plan tests => 2;
+    plan tests => 3;
     my @expect = (10, 11);
-    isnt SeriesInfo::NameParser::parse("show S01E1011 title")                , undef, "not undefined";
+    is_deeply SeriesInfo::NameParser::parse("show 1 1011 title")->{numbers}, \@expect, "simple";
     is_deeply SeriesInfo::NameParser::parse("show S01E1011 title")->{numbers}, \@expect, "S__E____";
+    is_deeply SeriesInfo::NameParser::parse("show.01x1011.title")->{numbers}, \@expect, "__x____";
 };
