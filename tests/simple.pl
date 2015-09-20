26ba632f2791b7120009f4d396af119e11668207
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 10:34:44 +0200

    move tests into subtest

--- tests/simple.pl
+++ tests/simple.pl
@@ -3,11 +3,13 @@ use strict;
 use warnings;
 use SeriesInfo::NameParse;
 
-use Test::More tests => 4;
+use Test::More tests => 1;
 
 my $s = "show 1 2 title";
-
-is SeriesInfo::NameParse::parse($s)->{show}  , "show"   , "get show from string";
-is SeriesInfo::NameParse::parse($s)->{season}, 1        , "get season number from string";
-is SeriesInfo::NameParse::parse($s)->{number}, 2        , "get episode number from string";
-is SeriesInfo::NameParse::parse($s)->{title} , "title"  , "get title from string";
+subtest $s => sub {
+    plan tests => 4;
+    is SeriesInfo::NameParse::parse($s)->{show}  , "show"   , "get show from string";
+    is SeriesInfo::NameParse::parse($s)->{season}, 1        , "get season number from string";
+    is SeriesInfo::NameParse::parse($s)->{number}, 2        , "get episode number from string";
+    is SeriesInfo::NameParse::parse($s)->{title} , "title"  , "get title from string";
+};
