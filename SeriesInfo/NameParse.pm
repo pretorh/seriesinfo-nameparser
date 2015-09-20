ab69f30d71444a288f8571ffe06081d2374ff624
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 10:47:16 +0200

    extract sub to build hash

--- SeriesInfo/NameParse.pm
+++ SeriesInfo/NameParse.pm
@@ -4,13 +4,15 @@ sub parse {
     my ($s) = @_;
 
     $s =~ /(.+?) (\d?) (\d?) (.+)$/;
-    return if (not $1);
+    return build($1, $2, $3, $4) if ($1);
+}
 
+sub build {
     return {
-        show => $1,
-        season => $2,
-        number => $3,
-        title => $4,
+        show => shift @_,
+        season => shift @_,
+        number => shift @_,
+        title => shift @_,
     };
 }
 
