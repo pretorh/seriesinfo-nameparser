cadaa84856742dc111c374746a4ab37c9ef6602b
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 10:23:43 +0200

    get the show name from the string

--- SeriesInfo/NameParse.pm
+++ SeriesInfo/NameParse.pm
@@ -1,3 +1,13 @@
 package SeriesInfo::NameParse;
 
+sub parse {
+    my ($s) = @_;
+
+    $s =~ /(.+?) /;
+
+    return {
+        show => $1,
+    };
+}
+
 1;
--- tests/simple.pl
+++ tests/simple.pl
@@ -2,3 +2,9 @@ BEGIN {@INC=("../",@INC)};
 use strict;
 use warnings;
 use SeriesInfo::NameParse;
+
+use Test::More tests => 1;
+
+my $s = "show 1 2 title";
+
+is SeriesInfo::NameParse::parse($s)->{show}  , "show"   , "get show from string";
